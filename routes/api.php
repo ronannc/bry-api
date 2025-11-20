53fa478402a975b027f66f33e2a811c37c98e948
Ronan Campos <ronannc1@yahoo.com.br>
2025-11-20 13:05:37 -0300

    feat: arquivo api de rota

---
+++ routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\PersonController;
+use Illuminate\Support\Facades\Route;
+
+Route::apiResource('companies', CompanyController::class);
+Route::apiResource('people', PersonController::class);
+
